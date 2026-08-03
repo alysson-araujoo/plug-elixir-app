38205e55d51143cf363dd1c49a35b038db9e01be
Alysson Araujo <alyssonaraujo.dev@gmail.com>
2026-08-03 17:23:35 -0300

    fix: Corrigindo o arquivo application.ex do diretório lib

--- lib/my_plug_app/application.ex
+++ lib/my_plug_app/application.ex
@@ -1,6 +1,4 @@
 defmodule MyPlugApp.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,9 @@ defmodule MyPlugApp.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: MyPlugApp.Worker.start_link(arg)
-      # {MyPlugApp.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: MyPlugApp.Router, options: [port: 4000]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MyPlugApp.Supervisor]
     Supervisor.start_link(children, opts)
   end
